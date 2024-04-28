78d7ea1ea7ee5f19a43f59a28e89028a44238f53
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-04-28 08:28:58 -0600

    second algorithm

---
+++ holamundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Hola Mundo! "<<'\n';
+    std::cout<<"Me gusta la pizza";
+    return 0;
+}
+
+// así se hace un comentario
+
+/*
+comentarios
+en varias lineas  
+*/
+
