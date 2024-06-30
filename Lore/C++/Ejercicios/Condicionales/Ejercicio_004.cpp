c5982f1eecf04392da04ebddbf0d008fb964e95e
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-06-29 20:05:33 -0600

    Add

--- Lore/C++/Ejercicios/Condicionales/Ejercicio_004.cpp
+++ Lore/C++/Ejercicios/Condicionales/Ejercicio_004.cpp
@@ -4,3 +4,19 @@ que debe pagar Si la compra es elegible para el descuento, muestra el monto tota
 descuento aplicado. Si no es elegible para el descuento, informa al usuario que no se aplica 
 el descuento y muestra la cantidad que debe pagar.*/
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    float compra;
+    cout << "Ingresa tu compra total: " << endl;
+    cin >> compra;
+
+    if (compra <= 300.0){
+        cout << "total a pagar: " << compra;
+    }
+    else{
+        compra = compra*0.8;
+        cout << "Total a pagar: " << compra << endl;
+    }
+}
--- Lore/C++/Ejercicios/Condicionales/Ejercicio_005.cpp
+++ Lore/C++/Ejercicios/Condicionales/Ejercicio_005.cpp
@@ -7,3 +7,30 @@ la siguiente manera:
 
 Crea un programa que pregunte al usuario las horas que ha trabajado y que le muestre su pago total 
 de acuerdo a lo que trabajó.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int salario_total;
+
+    int pago1 = 16; // si trabaja 40>
+    int pago2 = 20; // si trabaja 40<
+    
+    int horas;
+    cout << "Ingresa las horas trabajadas: " << endl;
+    cin >> horas;
+
+    if (horas <= 40){
+        salario_total = horas*pago1;
+        cout << "El salario es: " << salario_total;
+    }
+    else{
+        int salario1 = 40*16;
+        int salario2 = (horas-40)*20;
+        int salario_total_2 = salario1 + salario2;
+        cout << "El salario es: " << salario_total_2;
+    }
+    
+}
