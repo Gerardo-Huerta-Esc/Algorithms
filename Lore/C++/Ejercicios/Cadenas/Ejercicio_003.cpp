204dc364bc923013d7ee4b9ae9c66d7082998fbc
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-06-29 23:38:16 -0600

    Add

--- Lore/C++/Ejercicios/Cadenas/Ejercicio_003.cpp
+++ Lore/C++/Ejercicios/Cadenas/Ejercicio_003.cpp
@@ -1,3 +1,24 @@
 /*Escribir un programa que pregunte el nombre del usuario en la consola y después de que el 
 usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el 
 nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    
+    string nombre;
+    cout << "Ingresa tu nombre de usuario: " << endl;
+    cin >> nombre;
+    
+    transform(nombre.begin(),nombre.end(),nombre.begin(), ::toupper);
+  
+    int j = 0;
+    for (char i: nombre ){
+        j++;
+    }
+
+    cout << nombre << " Tiene: " << j << " letras ";
+}
