41a41b508c5b21b3a5ac530cea437ae2b1d61bae
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-07-01 06:57:57 -0600

    Add

---
+++ Lore/C++/Ejercicios/Funciones/Ejercicio_001.cpp
@@ -0,0 +1,15 @@
+/*Escribir una función que muestre por pantalla el saludo ¡Hola amiga! cada vez que
+ se la invoque.*/
+
+#include <iostream>
+using namespace std;
+
+void Hola(){
+    cout << "!!! "<< endl;
+    
+}
+
+int main(){
+    Hola();
+    Hola();
+}
