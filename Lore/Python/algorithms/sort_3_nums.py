73e93f45677ed045356761dd3bcf8169335e743f
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-07-01 20:52:01 -0600

    Add

---
+++ Lore/Python/algorithms/sort_3_nums.py
@@ -0,0 +1,27 @@
+#ordenar 3 números de mayor a menor
+
+num1 = 1
+num2 = 5
+num3 = -11
+
+def main():
+    if num1 > num2 and num1 > num3:
+        if num2 > num3:
+            print(f"{num1} > {num2} > {num3}")
+        if num3 > num2:
+            print(f"{num1} > {num3} > {num2}")
+    elif num2 > num1 and num2 > num3:
+        if num1 > num3:
+            print(f"{num2} > {num1} > {num3}")
+        if num3 > num1:
+            print(f"{num2} > {num3} > {num1}")
+    elif num3 > num1 and num3 > num2:
+        if num1 > num2:
+            print(f"{num3} > {num1} > {num2}")
+        if num2 > num1:
+            print(f"{num3} > {num2} > {num1}")
+
+if __name__ == '__main__' and num1 != num2 != num3:
+    main()
+
+
