46071ae4d3fe894b0c47abf68131c218520c2454
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-07-02 12:13:44 -0600

    Add

---
+++ Lore/Python/Ejercicios/Ejercicio_004.py
@@ -0,0 +1,14 @@
+"""
+Definición de Clases:
+
+Define una clase Empleado que tenga atributos comunes a todos los empleados, como nombre, edad y salario.
+Define una clase Desarrollador que herede de Empleado y tenga un atributo adicional lenguaje_de_programacion.
+Define una clase Gerente que herede de Empleado y tenga un atributo adicional departamento.
+Define una clase DesarrolladorGerente que herede de ambas clases Desarrollador y Gerente, combinando las responsabilidades de ambas.
+Métodos:
+
+Implementa métodos en las clases para mostrar la información de los empleados.
+Implementa un método en la clase DesarrolladorGerente para mostrar todas las responsabilidades.
+"""
+
+
