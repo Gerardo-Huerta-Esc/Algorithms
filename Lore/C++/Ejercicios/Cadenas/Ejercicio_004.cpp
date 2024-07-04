be4cb8d7fc114c4ce961ba18f00ae55a6175f0b6
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-07-04 07:51:09 -0600

    Add

--- Lore/C++/Ejercicios/Cadenas/Ejercicio_004.cpp
+++ Lore/C++/Ejercicios/Cadenas/Ejercicio_004.cpp
@@ -1,24 +1,9 @@
-/*Escribir un programa que pregunte el nombre del usuario en la consola y
-después de que el usuario lo introduzca muestre por pantalla <NOMBRE>tiene
-<n>letras, donde <NOMBRE>es el nombre de usuario en mayúsculas y <n>es
-el número de letras que tienen el nombre*/
+/*Escribir un programa que pida al usuario un número entero y muestre por
+pantalla si es par o impar.*/
 
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 int main(){
-    string name;
-    cout << "Insert a name: " << endl;
-    cin >> name;
     
-    int j =0;
-    for (char n: name ){
-        j++;
-    }
-
-    transform(name.begin(),name.end(),name.begin(),::toupper);
-    
-    cout << "The name: " << name << " have: " << j << " letters." << endl;
 }
