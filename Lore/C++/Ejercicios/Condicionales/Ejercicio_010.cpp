6021109861dbc45d1270c14806f5f0b557e349c5
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-07-11 14:02:41 -0600

    Add

--- Lore/C++/Ejercicios/Condicionales/Ejercicio_010.cpp
+++ Lore/C++/Ejercicios/Condicionales/Ejercicio_010.cpp
@@ -1,2 +1,17 @@
 /*Escribir un programa que pida al usuario un número entero y muestre por
 pantalla si es par o impar.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "ingresa un número: " << endl;
+    cin >> num;
+
+    if(num%2==0){
+        cout << "El número es par." << endl;
+    }else{
+        cout << "El número es impar. " << endl;
+    }
+}
