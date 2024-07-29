c41b7a01d8b0b17445d15b03da683c081d926d7e
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-07-29 08:48:54 -0600

    Add

---
+++ Lore/C++/Notes/Book/Chapter_1/001_atributos.cpp
@@ -0,0 +1,19 @@
+//Para declarar una clase
+
+using namespace std;
+#include <string>
+
+class CCuenta
+{                          // Un atributo declarado private es accesible solamente por los métodos de su propia clase.
+    private: string nombre; 
+    private: string cuenta;
+    private: double saldo;
+    private: double tipoDeInteres;
+};
+
+
+
+
+int main(){
+    return 0;
+}
