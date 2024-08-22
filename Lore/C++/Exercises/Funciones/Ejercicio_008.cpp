0aa1bdef4f6b84e28395048025df517826bae43c
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-08-22 07:56:16 -0600

    Add

--- Lore/C++/Exercises/Funciones/Ejercicio_008.cpp
+++ Lore/C++/Exercises/Funciones/Ejercicio_008.cpp
@@ -3,15 +3,33 @@ devuelva un diccionario con su media, varianza y desviación típica*/
 
 #include <iostream>
 #include <unordered_map>
+#include <vector>
+
 using namespace std; // aquí se tiene que utilizar esta estructura de datos que simula un diccionario de python
 
+vector<int> function(vector<int> arg) {
+    vector<int> arg_result;
+    int contador = 0;  // Inicializar la variable contador
+    for (int i = 0; i < arg.size()+1; i++) {
+        contador += i;
+    }
+    cout << "La media es :" <<  contador / arg.size() << endl;
+
+    return arg_result;
+}
+
+// Almenos hasta aquí ya logré escribir la parte de la media
+
 int main() {
-    unordered_map< string, int> diccionario;
+    vector<int> lista{1, 2, 3, 4, 5, 6, 7, 8, 9}; // lista que va a recibir
+    function(lista);
+
+    unordered_map<string, int> diccionario;
     diccionario["uno"] = 1;
     diccionario["dos"] = 2;
     diccionario["tres"] = 3;
 
-    std::cout << "El valor de 'dos' es: " << diccionario["dos"] << std::endl;
+    cout << "El valor de 'dos' es: " << diccionario["dos"] << endl;
 
     return 0;
 }
