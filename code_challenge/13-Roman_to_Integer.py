9c163bb646bd168ccf0ca5bbce552fa707b99d99
Gerardo-Huerta-Esc <129778722+Gerardo-Huerta-Esc@users.noreply.github.com>
2024-08-31 12:23:33 -0600

    Add

--- code_challenge/13-Roman_to_Integer.py
+++ code_challenge/13-Roman_to_Integer.py
@@ -73,14 +73,16 @@ It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 '''
 dictionary = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
 
-string = 'XXX'
+string = 'MCMXCIV'
 
 count = 0
-for i in string:
-    count += dictionary[i]
 
-
+for i in range(len(string)):
+    if i < len(string)-1 and dictionary[string[i]] < dictionary[string[i+1]]:
+        count -= dictionary[string[i]]
+    else:
+        count += dictionary[string[i]]
+    
+ 
 print(count)
 
-
-#print(dictionary['D'])
